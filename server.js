7b3264de5ea0da2f3871f1ea8dbc0ac9c13daacb
ocastorena <omarcastorena11@gmail.com>
2025-03-06 09:35:41 -0700

    ECAB-5 Add database connection and initial config

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const { query } = require("./db/db.js");
+
 const app = express();
 const port = 3000;
+
+query("SELECT NOW()");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
