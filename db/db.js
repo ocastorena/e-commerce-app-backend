6f68ffa7b730a76b12fc1d9f8cc75f0f79412c6e
Omar Castorena <48456501+ocastorena@users.noreply.github.com>
2025-03-06 09:37:40 -0700

    ECAB-5-Connect-the-app-and-database (#2)
    * ECAB-5 Install node-postgres and dotenv
    
    * ECAB-5 Add database connection and initial config

---
+++ db/db.js
@@ -0,0 +1,31 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+// PostgreSQL connection configuration
+const pool = new Pool();
+
+// Test database connection
+// pool.connect((err, client, release) => {
+//   if (err) {
+//     return console.error("Error acquirung client", err.stack);
+//   }
+//   client.query("SELECT NOW()", (err, result) => {
+//     release();
+//     if (err) {
+//       return console.error("Error executing query", err.stack);
+//     }
+//     console.log("Connected to the database:", result.rows);
+//   });
+// });
+
+const query = async (text, params) => {
+  const start = Date.now();
+  const res = await pool.query(text, params);
+  const duration = Date.now() - start;
+  console.log("executed query", { text, duration, rows: res.rowCount });
+  return res;
+};
+
+module.exports = {
+  query,
+};
