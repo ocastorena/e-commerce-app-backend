04b75ec2e0c95d02c159ff761abe749204523975
ocastorena <omarcastorena11@gmail.com>
2025-03-11 08:35:06 -0700

    ECAB-8 Configure Passportjs and Express-session

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const session = require("express-session");
+const passport = require("./config/passportConfig");
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Express session configuration
+app.use(
+  session({
+    secret: "your_secret_key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Initialize Passport and session
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Add routes
 app.use("/", userRoutes);
 
 app.listen(port, () => {
