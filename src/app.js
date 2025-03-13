26f2f4b373bd71e79c08a52139cb63f5c9578c2f
ocastorena <omarcastorena11@gmail.com>
2025-03-13 09:20:30 -0700

    ECAB-14 Configure swagger

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const session = require("express-session");
+const swaggerSpec = require("./config/swaggerConfig");
+const swaggerUi = require("swagger-ui-express");
 const passport = require("./config/passportConfig");
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
@@ -30,6 +32,15 @@ app.use("/", userRoutes);
 app.use("/", productRoutes);
 app.use("/", cartRoutes);
 app.use("/", orderRoutes);
+
+// serve swagger spec
+app.get("/swagger.json", function (req, res) {
+  res.setHeader("Content-Type", "application/json");
+  res.send(swaggerSpec);
+});
+
+// serve swagger ui
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(port, () => {
   console.log(`Server app listening on port ${port}`);
