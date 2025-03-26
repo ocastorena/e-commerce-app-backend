0e5f4b1f25fae0d4e575d785fe54f72c8c9327a2
ocastorena <omarcastorena11@gmail.com>
2025-03-26 16:21:20 -0700

    ECAB-15 Add cors with default settings

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const session = require("express-session");
+const cors = require("cors");
+
 const swaggerSpec = require("./config/swaggerConfig");
 const swaggerUi = require("swagger-ui-express");
+
 const passport = require("./config/passportConfig");
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
@@ -10,6 +13,8 @@ const orderRoutes = require("./routes/orderRoutes");
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
